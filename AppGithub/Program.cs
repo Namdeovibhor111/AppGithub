0e1cf3f80462526b1f11aac0bae1c0b9cf363faa
Namdeovibhor111 <lordvib3@gmail.com>
2022-08-11 09:33:11 +0530

    Add project files.

---
+++ AppGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
