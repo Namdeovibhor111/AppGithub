62ad38a9148cfc0f66f63847edbf5e2e63375ee4
Namdeovibhor111 <lordvib3@gmail.com>
2022-08-11 10:03:45 +0530

    2nd line added

--- AppGithub/Program.cs
+++ AppGithub/Program.cs
@@ -7,6 +7,7 @@ namespace AppGithub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Today is Thursday");
         }
     }
 }
